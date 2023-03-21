53f1a308fdc014edff89a428f49eb82a59ae28f1
kyeongju <ainimo2@gmail.com>
2023-03-21 12:20:42 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1098,7 +1098,7 @@ resource "aws_iam_instance_profile" "this" {
   # for_each = { for k, v in var.metric_alarms : k => v if local.create && var.create_scaling_policy }
   # for_each = { for k, v in var.metric_alarms : k => v if local.create && var.create_scaling_policy && var.create_metric_alarm }
   # for_each = { for k, v in var.metric_alarms : k => v }
-  for_each = { for k, v in var.metric_alarms : k => v if local.create && var.create_metric_alarm }
+  for_each = { for k, v in var.metric_alarms : k => v if local.create}
   
   alarm_name          = try(each.value.name, each.key)
   comparison_operator = try(each.value.comparison_operator, "GreaterThanThreshold")
