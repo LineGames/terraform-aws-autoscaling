bbc647011199f84c56a171aba3600a99f9f6e44f
kyeongju <ainimo2@gmail.com>
2023-03-21 18:37:48 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1116,6 +1116,6 @@ resource "aws_iam_instance_profile" "this" {
 
   }
 
-  alarm_actions = [aws_autoscaling_policy.this["scaleout"].arn] && try(each.value.alarm_actions)
+  alarm_actions = [aws_autoscaling_policy.this["scaleout"].arn, try(each.value.alarm_actions)]
   #alarm_actions = try(each.value.alarm_actions)
 }
